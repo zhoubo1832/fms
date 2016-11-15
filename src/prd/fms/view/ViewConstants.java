3d54f74842753c47341bf0bb3aa11499ce91c4fa
zhoubo1832 <zhoubo1832@sina.com>
2016-11-15 13:29:53 +0900

    Create ViewConstants.java

---
+++ src/prd/fms/view/ViewConstants.java
@@ -0,0 +1,9 @@
+package prd.fms.view;
+
+public class ViewConstants {
+
+	public static String MAIN_FRAME_TITLE = "File Manage System";
+	public static String MAIN_TREE_ROOT_NODE = "Root";
+	public static String MAIN_TREE_DUMMY_NODE = "Dummy";
+	
+}
