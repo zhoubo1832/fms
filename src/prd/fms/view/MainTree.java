f839273682e14db9dacaa27af4a308b72fa3e791
zhoubo1832 <zhoubo1832@sina.com>
2016-11-15 13:28:41 +0900

    Create MainTree.java

---
+++ src/prd/fms/view/MainTree.java
@@ -0,0 +1,28 @@
+package prd.fms.view;
+
+import javax.swing.JTree;
+import javax.swing.tree.TreeSelectionModel;
+
+import prd.fms.controller.TreeNodeSelectionController;
+import prd.fms.controller.TreeNodeWillExpandController;
+import prd.fms.module.TreeNodeModule;
+
+public class MainTree extends JTree{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public MainTree() {
+		super(TreeNodeModule.getRootNodes());
+		this.setRootVisible(true);
+		this.setShowsRootHandles(false);
+		
+		this.setCellRenderer(new MainTreeCellRender());
+		this.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+		
+		this.addTreeSelectionListener(new TreeNodeSelectionController());
+		this.addTreeWillExpandListener(new TreeNodeWillExpandController());
+	}
+}
