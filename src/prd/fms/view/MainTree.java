d343afd331444f4ac26dc72347633fe3ac0ce5d6
zhoubo1832 <zhoubo1832@sina.com>
2016-11-18 11:55:41 +0900

    modified

--- src/prd/fms/view/MainTree.java
+++ src/prd/fms/view/MainTree.java
@@ -3,6 +3,7 @@ package prd.fms.view;
 import javax.swing.JTree;
 import javax.swing.tree.TreeSelectionModel;
 
+import prd.fms.controller.TreeNodeMouseController;
 import prd.fms.controller.TreeNodeSelectionController;
 import prd.fms.controller.TreeNodeWillExpandController;
 import prd.fms.module.TreeNodeModule;
@@ -18,14 +19,15 @@ public class MainTree extends JTree{
 
 	public MainTree() {
 		super(TreeNodeModule.getRootNodes());
-		this.setRootVisible(true);
-		this.setShowsRootHandles(false);
+		this.setRootVisible(false);
+		this.setShowsRootHandles(true);
 		
 		this.setCellRenderer(new MainTreeCellRender());
 		this.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
 		
 		this.addTreeSelectionListener(new TreeNodeSelectionController());
 		this.addTreeWillExpandListener(new TreeNodeWillExpandController());
+		this.addMouseListener(new TreeNodeMouseController());
 		MainTree.instance = this;
 	}
 }
