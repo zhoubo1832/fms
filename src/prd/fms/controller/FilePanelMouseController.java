6645da9eab329dd4c6a270991006809442e5eec0
zhoubo1832 <zhoubo1832@sina.com>
2016-11-30 18:39:34 +0900

    add shift key selection function

--- src/prd/fms/controller/FilePanelMouseController.java
+++ src/prd/fms/controller/FilePanelMouseController.java
@@ -40,20 +40,70 @@ public class FilePanelMouseController implements MouseListener{
 		// one click	
 			FilePanel panel = (FilePanel)e.getSource();
 			boolean flgControl = FilePanelKeyController.controlKeyPressed;
-			if(!flgControl) {
+			boolean flgShift = FilePanelKeyController.shiftKeyPressed;
+			if(!flgControl && !flgShift) {
 				Container container = panel.getParent();
 				for( Component p : container.getComponents()) {
 					if(p != panel) {
 						((JComponent) p).setBorder(null);
 						p.setBackground(null);
+					} else {
+						panel.requestFocus();
+						panel.setBackground(new Color(219,243,146));
+						panel.setBorder(BorderFactory.createRaisedSoftBevelBorder());
 					}
 				}
 
+			} else if(flgShift) {
+				Container container = panel.getParent();
+				int i = 0;
+				for( ; i < container.getComponentCount(); i++) {
+					Component p = container.getComponent(i);
+					if(p == panel) {
+						break;
+					}
+				}
+				
+				int j = 0;
+				for( ; j < container.getComponentCount(); j++) {
+					Component p = container.getComponent(j);
+					if(((JComponent) p).getBorder() != null && j != i) {
+						break;
+					}
+				}
+				
+				if(j >= container.getComponentCount()) {
+					j = i;
+				}
+				
+				System.out.println("i=" + i);
+				System.out.println("j=" + j);
+				
+				int begin=i;
+				int end=j;
+				if(i > j) {
+					begin = j;
+					end = i;
+				}
+				
+				while(begin <= end) {
+					Component p = container.getComponent(begin);
+					p.requestFocus();
+					p.setBackground(new Color(219,243,146));
+					((JComponent) p).setBorder(BorderFactory.createRaisedSoftBevelBorder());
+					begin++;
+				}
+			} else {
+				panel.requestFocus();
+				panel.setBackground(new Color(219,243,146));
+				panel.setBorder(BorderFactory.createRaisedSoftBevelBorder());
 			}
+//			Container container = panel.getParent();
+//			container.get
 			// request focus for file panel, and focusGained method will be called automatically
-			panel.requestFocus();
-			panel.setBackground(new Color(219,243,146));
-			panel.setBorder(BorderFactory.createRaisedSoftBevelBorder());
+//			panel.requestFocus();
+//			panel.setBackground(new Color(219,243,146));
+//			panel.setBorder(BorderFactory.createRaisedSoftBevelBorder());
 			
 			ToolbarPanel.instance.setRenameButtonEnabled(true);
 			ToolbarPanel.instance.setCopyButtonEnabled(true);
