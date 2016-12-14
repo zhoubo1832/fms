9f688e67ba8d54e45696de577ce46be8c3d3ecee
zhoubo1832 <zhoubo1832@sina.com>
2016-12-14 11:45:21 +0900

    add tag dialog

--- src/prd/fms/executor/FilePanelMouseExecutor.java
+++ src/prd/fms/executor/FilePanelMouseExecutor.java
@@ -5,6 +5,7 @@ import java.awt.Component;
 import java.awt.Container;
 import java.awt.event.MouseEvent;
 import java.io.File;
+import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.JComponent;
@@ -12,93 +13,110 @@ import javax.swing.JPanel;
 
 import prd.fms.controller.BaseMouseListener;
 import prd.fms.model.FileNodeModel;
+import prd.fms.model.TagModel;
 import prd.fms.view.FilePanel;
 import prd.fms.view.InfoBarPanel;
+import prd.fms.view.MainFrame;
+import prd.fms.view.TagDialog;
 import prd.fms.view.ToolbarPanel;
 
 public class FilePanelMouseExecutor extends BaseMouseListener{
 
 	@Override
 	protected void executeClicked(MouseEvent e) {
-		int n = e.getClickCount();
-		// double click
-		if(n == 2){
-			FilePanel panel = (FilePanel)e.getSource();
-			File file = panel.getFile();
-			FileNodeModel.openFileNode(file);
-			
-			ToolbarPanel.instance.setRenameButtonEnabled(false);
-			ToolbarPanel.instance.setCopyButtonEnabled(false);
-			
-		} else {
-		// one click	
-			FilePanel panel = (FilePanel)e.getSource();
-			boolean flgControl = FilePanelKeyExecutor.controlKeyPressed;
-			boolean flgShift = FilePanelKeyExecutor.shiftKeyPressed;
-			if(!flgControl && !flgShift) {
-				Container container = panel.getParent();
-				for( Component p : container.getComponents()) {
-					if(p != panel) {
-						((JComponent) p).setBorder(null);
-						p.setBackground(null);
-					} else {
-						panel.requestFocus();
-						panel.setBackground(new Color(219,243,146));
-						panel.setBorder(BorderFactory.createRaisedSoftBevelBorder());
-					}
-				}
-
-			} else if(flgShift) {
-				Container container = panel.getParent();
-				int i = 0;
-				for( ; i < container.getComponentCount(); i++) {
-					Component p = container.getComponent(i);
-					if(p == panel) {
-						break;
-					}
-				}
+		if(e.getButton() == 1) {
+			int n = e.getClickCount();
+			// double click
+			if(n == 2){
+				doDoubleClick(e);
 				
-				int j = 0;
-				for( ; j < container.getComponentCount(); j++) {
-					Component p = container.getComponent(j);
-					if(((JComponent) p).getBorder() != null && j != i) {
-						break;
-					}
-				}
-				
-				if(j >= container.getComponentCount()) {
-					j = i;
-				}
-				
-				int begin=i;
-				int end=j;
-				if(i > j) {
-					begin = j;
-					end = i;
-				}
-				
-				while(begin <= end) {
-					Component p = container.getComponent(begin);
-					p.requestFocus();
-					p.setBackground(new Color(219,243,146));
-					((JComponent) p).setBorder(BorderFactory.createRaisedSoftBevelBorder());
-					begin++;
-				}
 			} else {
-				panel.requestFocus();
-				panel.setBackground(new Color(219,243,146));
-				panel.setBorder(BorderFactory.createRaisedSoftBevelBorder());
+			// one click	
+				doOneClick(e);
 			}
-			
-			ToolbarPanel.instance.setRenameButtonEnabled(true);
-			ToolbarPanel.instance.setCopyButtonEnabled(true);
-			
-			// display file detailed information in information panel
-			InfoBarPanel.instance.setFileInfoLabel(panel.getFile().getPath());
+		} else if(e.getButton() == 3) {
+			FilePanel panel = (FilePanel)e.getSource();
+			String filePath = panel.getFile().getPath();
+			List<Object> list = TagModel.getFileTags(filePath);
+			new TagDialog(MainFrame.instance, "Tags", true, filePath, list);
 		}
-
 	
 		
+	}
+
+	private void doOneClick(MouseEvent e) {
+		FilePanel panel = (FilePanel)e.getSource();
+		boolean flgControl = FilePanelKeyExecutor.controlKeyPressed;
+		boolean flgShift = FilePanelKeyExecutor.shiftKeyPressed;
+		if(!flgControl && !flgShift) {
+			Container container = panel.getParent();
+			for( Component p : container.getComponents()) {
+				if(p != panel) {
+					((JComponent) p).setBorder(null);
+					p.setBackground(null);
+				} else {
+					panel.requestFocus();
+					panel.setBackground(new Color(219,243,146));
+					panel.setBorder(BorderFactory.createRaisedSoftBevelBorder());
+				}
+			}
+
+		} else if(flgShift) {
+			Container container = panel.getParent();
+			int i = 0;
+			for( ; i < container.getComponentCount(); i++) {
+				Component p = container.getComponent(i);
+				if(p == panel) {
+					break;
+				}
+			}
+			
+			int j = 0;
+			for( ; j < container.getComponentCount(); j++) {
+				Component p = container.getComponent(j);
+				if(((JComponent) p).getBorder() != null && j != i) {
+					break;
+				}
+			}
+			
+			if(j >= container.getComponentCount()) {
+				j = i;
+			}
+			
+			int begin=i;
+			int end=j;
+			if(i > j) {
+				begin = j;
+				end = i;
+			}
+			
+			while(begin <= end) {
+				Component p = container.getComponent(begin);
+				p.requestFocus();
+				p.setBackground(new Color(219,243,146));
+				((JComponent) p).setBorder(BorderFactory.createRaisedSoftBevelBorder());
+				begin++;
+			}
+		} else {
+			panel.requestFocus();
+			panel.setBackground(new Color(219,243,146));
+			panel.setBorder(BorderFactory.createRaisedSoftBevelBorder());
+		}
+		
+		ToolbarPanel.instance.setRenameButtonEnabled(true);
+		ToolbarPanel.instance.setCopyButtonEnabled(true);
+		
+		// display file detailed information in information panel
+		InfoBarPanel.instance.setFileInfoLabel(panel.getFile().getPath());
+	}
+
+	private void doDoubleClick(MouseEvent e) {
+		FilePanel panel = (FilePanel)e.getSource();
+		File file = panel.getFile();
+		FileNodeModel.openFileNode(file);
+		
+		ToolbarPanel.instance.setRenameButtonEnabled(false);
+		ToolbarPanel.instance.setCopyButtonEnabled(false);
 	}
 
 	@Override
--- src/prd/fms/executor/FileTableMouseExecutor.java
+++ src/prd/fms/executor/FileTableMouseExecutor.java
@@ -2,13 +2,17 @@ package prd.fms.executor;
 
 import java.awt.event.MouseEvent;
 import java.io.File;
+import java.util.List;
 
 import javax.swing.JTable;
 
 import prd.fms.controller.BaseMouseListener;
 import prd.fms.model.FileNodeModel;
+import prd.fms.model.TagModel;
 import prd.fms.view.FileListTableModel;
 import prd.fms.view.InfoBarPanel;
+import prd.fms.view.MainFrame;
+import prd.fms.view.TagDialog;
 import prd.fms.view.ToolbarPanel;
 
 public class FileTableMouseExecutor extends BaseMouseListener{
@@ -21,32 +25,46 @@ public class FileTableMouseExecutor extends BaseMouseListener{
 	
 	@Override
 	protected void executeClicked(MouseEvent e) {
-		int n = e.getClickCount();
-		// double click
-		if( n == 2) {
-			int rowNum = fileTable.getSelectedRow();
+		if(e.getButton() == 1) {
+			int n = e.getClickCount();
+			// double click
+			if( n == 2) {
+				int rowNum = fileTable.getSelectedRow();
+				if(rowNum == -1) {
+					return;
+				}
+				rowNum = fileTable.convertRowIndexToModel(rowNum);
+				FileListTableModel model = (FileListTableModel)fileTable.getModel();
+				File file = model.getFiles().get(rowNum);
+				FileNodeModel.openFileNode(file);
+				ToolbarPanel.instance.setRenameButtonEnabled(false);
+				ToolbarPanel.instance.setCopyButtonEnabled(false);
+			} else {
+			// one click	
+				int rowNum = fileTable.getSelectedRow();
+				if(rowNum == -1) {
+					return;
+				}
+				rowNum = fileTable.convertRowIndexToModel(rowNum);
+				FileListTableModel model = (FileListTableModel)fileTable.getModel();
+				String path = (model.getFiles().get(rowNum)).getPath();
+				InfoBarPanel.instance.setFileInfoLabel(path);
+				ToolbarPanel.instance.setRenameButtonEnabled(true);
+				ToolbarPanel.instance.setCopyButtonEnabled(true);
+			}
+		} else if(e.getButton() == 3) {
+			int rowNum = fileTable.rowAtPoint(e.getPoint());
 			if(rowNum == -1) {
 				return;
 			}
+			fileTable.setRowSelectionInterval(rowNum, rowNum);
 			rowNum = fileTable.convertRowIndexToModel(rowNum);
+			
 			FileListTableModel model = (FileListTableModel)fileTable.getModel();
-			File file = model.getFiles().get(rowNum);
-			FileNodeModel.openFileNode(file);
-			ToolbarPanel.instance.setRenameButtonEnabled(false);
-			ToolbarPanel.instance.setCopyButtonEnabled(false);
-		} else {
-		// one click	
-			int rowNum = fileTable.getSelectedRow();
-			if(rowNum == -1) {
-				return;
-			}
-			rowNum = fileTable.convertRowIndexToModel(rowNum);
-			FileListTableModel model = (FileListTableModel)fileTable.getModel();
-			String path = (model.getFiles().get(rowNum)).getPath();
-			InfoBarPanel.instance.setFileInfoLabel(path);
-			ToolbarPanel.instance.setRenameButtonEnabled(true);
-			ToolbarPanel.instance.setCopyButtonEnabled(true);
-		}		
+			String filePath = (model.getFiles().get(rowNum)).getPath();
+			List<Object> list = TagModel.getFileTags(filePath);
+			new TagDialog(MainFrame.instance, "Tags", true, filePath, list);
+		}
 	}
 
 }
